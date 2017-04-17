6809ea2fa4d6c41a64502ad0cdeeb8a4a02f353f
Mac Pro <macpro@Macs-MBP.fios-router.home>
2017-04-16 20:23:39 -0400

    First Commit by Natalia

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Twitter::Application.config.secret_key_base = 'ef7e25020a077d15380788d544569c99f6065148c1d1d51aa246aaf13c8fb7b47940610a13e02544008ff884f72450671b9edf0c3f45a33c1bfdc7e02b3ff5a9'
